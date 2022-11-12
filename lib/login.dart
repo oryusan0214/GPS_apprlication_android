3a27481bad46b2b73aa641cd5e355f3d1fe69ffe
Oryu.Y <72446055+oryusan0214@users.noreply.github.com>
2022-11-12 09:57:15 +0900

    ADD: ログイン画面の実装

---
+++ lib/login.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_test/main.dart';
+import 'map.dart';
+
+class loginValidate extends StatefulWidget {
+  const loginValidate({Key? key}) : super(key: key);
+
+  @override
+  State<loginValidate> createState() => _loginValidateState();
+}
+
+class _loginValidateState extends State<loginValidate> {
+  bool _isObscure = true;
+
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('JboyApp'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(30.0),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'ユーザー名が入力されていません!';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'ユーザー名を入力してください',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'パスワードが入力されていません!';
+                      }
+                      return null;
+                    },
+                    obscureText: _isObscure,
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        suffixIcon: IconButton(
+                            icon: Icon(_isObscure
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                            onPressed: () {
+                              setState(() {
+                                _isObscure = !_isObscure;
+                              });
+                            })),
+                  ),
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Processing Data')),
+                        );
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> Map()));
+                      }
+                    }, child: Text('ログイン')),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,20 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_test/login.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(const MyApp());\
+import 'map.dart';
 
-class MyApp extends StatefulWidget {
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'test',
+      home: loginValidate(),
     );
   }
 }
